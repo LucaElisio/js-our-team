bd5bb06e61ceb7feda28d4b3eea57925b301050a
LucaElisio <personal.elix@gmail.com>
2024-04-11 14:44:12 +0200

    feat: make array of objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,42 @@
 // BONUS 1 :stella2::
 // Trasformare la stringa foto in una immagine effettiva
 // BONUS 2 :stella2: :stella2:
-// Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
+// Organizzare i singoli membri in card/schede. 
+
+
+// Creo array di oggetti
+const members = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg"
+    }, 
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg"
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg"
+    }, 
+];
+
+console.log(members);
+
